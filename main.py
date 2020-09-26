4f79a165e0a4d1849c3373f76fe8c60e8b457525
Skogen <muumlover@live.com>
2020-09-26 21:56:53 +0800

    scan row col

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import pyb
 
 micropython.alloc_emergency_exception_buf(100)
 
-ENABLE_USB = False
+ENABLE_USB = True
 ROW = ['B4', 'B5', 'B6', 'B7', 'B8', 'A1']
 COL = ['B12', 'B13', 'B14', 'B15', 'A8', 'A9', 'A10', 'A15', 'B9', 'A7', 'A6', 'B0', 'A5', 'A2', 'A3', 'A4']
 
 KEY_MAP = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [0, 0, 0, 0, 0, 0x29],
+    [0] * len(COL),
+    [0] * len(COL),
+    [0] * len(COL),
+    [0] * len(COL),
+    [0] * len(COL),
+    [0] * 5 + [0x29] + [0] * (len(COL) - 6),
 ]
 
 
@@ -30,11 +30,16 @@ class KeyBoard:
 
         for row_pin in self.row_pins:
             row_pin.init(pyb.Pin.OUT_OD)
+            row_pin.high()
         for col_pin in self.col_pins:
             col_pin.init(pyb.Pin.IN, pull=pyb.Pin.PULL_UP)
 
     def _get_index(self, code):
         return int(code / 8) + self.HEAD, 1 << (code % 8)
+
+    def _check(self, code):
+        byte_index, bit_index = self._get_index(code)
+        return self.buf[byte_index] & bit_index
 
     def _set(self, code):
         byte_index, bit_index = self._get_index(code)
@@ -45,11 +50,18 @@ class KeyBoard:
         self.buf[byte_index] = self.buf[byte_index] & ~bit_index
 
     def _scan(self):
-        if pyb.Pin('A9').value() == 0:
-            self._set(0x29)
+        for row, row_pin in enumerate(self.row_pins):
+            row_pin.low()
+            for col, col_pin in enumerate(self.col_pins):
+                if col_pin.value() == 0:
+                    self._set(KEY_MAP[row][col])
+                else:
+                    self._reset(KEY_MAP[row][col])
+            row_pin.high()
+
+        if self._check(0x29):
             pyb.LED(1).on()
         else:
-            self._reset(0x29)
             pyb.LED(1).off()
 
     def run(self):
@@ -58,14 +70,6 @@ class KeyBoard:
             if ENABLE_USB:
                 self.hid.send(self.buf)
 
-
-def on_target():
-    global ENABLE_USB
-    if not ENABLE_USB:
-        ENABLE_USB = True
-
-
-ext = pyb.ExtInt(pyb.Pin('A0'), pyb.ExtInt.IRQ_RISING_FALLING, pyb.Pin.PULL_UP, on_target)
 
 if __name__ == '__main__':
     kb = KeyBoard()
